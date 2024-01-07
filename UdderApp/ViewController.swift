82724b485ed30b2441664fc776fb269be8eb9bdd
M7moodAyman96 <mahmood.ayman96@gmail.com>
2024-01-07 12:12:33 +0300

    Adding a new function to viewWillAppear

--- UdderApp/ViewController.swift
+++ UdderApp/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
     }
 
     override func viewWillAppear(_ animated: Bool) {
-        <#code#>
+        //Adding a new function to viewWillAppear
     }
 
 }
